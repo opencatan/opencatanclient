5dcce93b8963debf45d80f20314cbb6fff2bfeab
Ben Brown <Ben@Bens-MacBook-Pro-6.local>
2019-02-16 23:42:47 -0800

    incorporate Matti's number graphics

--- src/Board.js
+++ src/Board.js
@@ -7,11 +7,24 @@ import brick from './images/brick.png';
 import desert from './images/desert.png';
 import water from './images/water.png';
 
+import imgNum2 from './images/img_num_02.png';
+import imgNum3 from './images/img_num_03.png';
+import imgNum4 from './images/img_num_04.png';
+import imgNum5 from './images/img_num_05.png';
+import imgNum6 from './images/img_num_06.png';
+import imgNum8 from './images/img_num_08.png';
+import imgNum9 from './images/img_num_09.png';
+import imgNum10 from './images/img_num_10.png';
+import imgNum11 from './images/img_num_11.png';
+import imgNum12 from './images/img_num_12.png';
+
 class Board extends Component {
 
   constructor(props) {
     super(props);
     this.tile_offset = 0.22 * this.props.tile_height;
+    this.number_tile_width = 0.4 * this.props.tile_width;
+    this.number_tile_height = 0.94 * this.number_tile_width;
     this.state = {"mouseDownInCanvas": false,
                   "canvasMouseX": 0,
                   "canvasMouseY": 0};
@@ -29,7 +42,20 @@ class Board extends Component {
       "sheep": this.refs.sheep_tile,
       "desert": this.refs.desert_tile,
       "water": this.refs.water_tile,
+
+      "num2": this.refs.imgNum2,
+      "num3": this.refs.imgNum3,
+      "num4": this.refs.imgNum4,
+      "num5": this.refs.imgNum5,
+      "num6": this.refs.imgNum6,
+      "num8": this.refs.imgNum8,
+      "num9": this.refs.imgNum9,
+      "num10": this.refs.imgNum10,
+      "num11": this.refs.imgNum11,
+      "num12": this.refs.imgNum12,
     }
+
+
     this.updateCanvas();
   }
 
@@ -60,22 +86,16 @@ class Board extends Component {
     const x = this.props.tile_width * column + this.props.tile_width * 0.5 * (row % 2 === 1);
     const y = (this.props.tile_height - this.tile_offset) * row;
 
-    if (tile && tile.resource_type && this.imgs[tile.resource_type]) {
+    if (tile && tile.resource_type && this.imgs[tile.resource_type] && tile.resource_number) {
       const img = this.imgs[tile.resource_type];
       context.drawImage(img, x, y, this.props.tile_width, this.props.tile_height);
-      context.font = "30px Arial";
-      const cx = x + this.props.tile_width / 2;
-      const cy = y + this.props.tile_height / 2;
 
-      //draw circle
-      context.beginPath();
-      context.arc(cx, cy, 20, 0, 2 * Math.PI);
-      context.fillStyle = "white";
-      context.fill();
-
-      //draw number
-      context.fillStyle = "black";
-      context.fillText(tile.resource_number, cx - 8, cy + 10);
+      if (tile.resource_number !== 7) {
+        const imgNumber = this.imgs["num" + tile.resource_number];
+        const cx = x + this.props.tile_width / 2 - this.number_tile_width / 2;
+        const cy = y + this.props.tile_height / 2 - this.number_tile_height / 2;
+        context.drawImage(imgNumber, cx, cy, this.number_tile_width, this.number_tile_width);
+      }
     }
   }
 
@@ -134,6 +154,17 @@ class Board extends Component {
         <img ref="brick_tile" alt="" src={brick} height="0" width="0" className="hidden"/>
         <img ref="desert_tile" alt="" src={desert} height="0" width="0" className="hidden"/>
         <img ref="water_tile" alt="" src={water} height="0" width="0" className="hidden"/>
+
+        <img ref="imgNum2" alt="" src={imgNum2} height="0" width="0" className="hidden"/>
+        <img ref="imgNum3" alt="" src={imgNum3} height="0" width="0" className="hidden"/>
+        <img ref="imgNum4" alt="" src={imgNum4} height="0" width="0" className="hidden"/>
+        <img ref="imgNum5" alt="" src={imgNum5} height="0" width="0" className="hidden"/>
+        <img ref="imgNum6" alt="" src={imgNum6} height="0" width="0" className="hidden"/>
+        <img ref="imgNum8" alt="" src={imgNum8} height="0" width="0" className="hidden"/>
+        <img ref="imgNum9" alt="" src={imgNum9} height="0" width="0" className="hidden"/>
+        <img ref="imgNum10" alt="" src={imgNum10} height="0" width="0" className="hidden"/>
+        <img ref="imgNum11" alt="" src={imgNum11} height="0" width="0" className="hidden"/>
+        <img ref="imgNum12" alt="" src={imgNum12} height="0" width="0" className="hidden"/>
       </div>
     );
   }
