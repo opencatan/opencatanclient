87de59ce7bd57f0b30a4e33667b01c18cb754724
Ben Brown <Ben@Bens-MacBook-Pro-6.local>
2019-02-20 20:41:44 -0800

    add the ability to use dev server from client

--- src/App.js
+++ src/App.js
@@ -3,16 +3,28 @@ import Board from './Board';
 import ControlArea from './ControlArea';
 import './App.css';
 
+const liveServerURL = 'https://opencatanserver.herokuapp.com/';
+const devServerURL = 'http://127.0.0.1:5000/';
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       game: {},
+      shouldUseLiveServer: true,
     }
   }
 
-  componentDidMount() {
-    fetch('https://opencatanserver.herokuapp.com/')
+  serverURL() {
+    return this.state.shouldUseLiveServer ? liveServerURL : devServerURL;
+  }
+
+  handleInputChange(e) {
+    this.setState({shouldUseLiveServer: e.target.checked});
+  }
+
+  loadData() {
+    fetch(this.serverURL())
       .then(response => response.json()).then((data) => {
         console.log(data);
         this.setState({ game: data });
@@ -22,8 +34,12 @@ class App extends Component {
       });
   }
 
+  componentDidMount() {
+    this.loadData();
+  }
+
   generateNewBoard(min, max) {
-    fetch("https://opencatanserver.herokuapp.com/generate/" + min + "/" + max)
+    fetch(this.serverURL() + "generate/" + min + "/" + max)
       .then(response => response.json()).then((data) => {
         console.log(data);
         this.setState({ game: data });
@@ -37,6 +53,10 @@ class App extends Component {
       <div className="App">
         <Board tile_width={150} tile_height={174} game_state={this.state.game}/>
         <ControlArea game_state={this.state.game} onGenerateNewBoard={(min, max) => this.generateNewBoard(min, max)}/>
+
+        <label>Use live server: </label>
+        <input name="Use live server" type="checkbox" checked={this.state.shouldUseLiveServer} onChange={(e) => this.handleInputChange(e)} />
+        <button onClick={() => this.loadData()}>Reload data</button>
       </div>
     );
   }
